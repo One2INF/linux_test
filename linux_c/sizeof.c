76866066b16f2fb8d95ccbf233ebcd5c9072922a
ZhengJie Shi <goodbob256@gmail.com>
2022-02-09 17:53:23 +0800

    mod: 添加 long 类型打印

--- linux_c/sizeof.c
+++ linux_c/sizeof.c
@@ -2,7 +2,13 @@
 
 int main(void)
 {
-  printf("int size: %lu\r\n", sizeof(int));
-  printf("int* size: %lu\r\n", sizeof(int*));
+  printf("int\t\tsize: %lu\r\n", sizeof(int));
+  printf("int*\t\tsize: %lu\r\n", sizeof(int*));
+
+  printf("long\t\t\tsize: %lu\r\n", sizeof(10L));
+  printf("unsigned long\t\tsize: %lu\r\n", sizeof 10UL);
+  printf("long long\t\tsize: %lu\r\n", sizeof 10LL);
+  printf("unsigned long long\tsize: %lu\r\n", sizeof 10ULL);
+
   return 0;
 }
