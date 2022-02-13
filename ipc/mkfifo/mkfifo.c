d2007a4c304ae6d57b4848215601e1b2b3464089
ZhengJie Shi <goodbob256@gmail.com>
2022-02-13 17:22:22 +0800

    mkfifo.c: change the open flag of pipe fifo.

--- ipc/mkfifo/mkfifo.c
+++ ipc/mkfifo/mkfifo.c
@@ -19,49 +19,58 @@ int main(void)
 {
   const int BUFFER_SIZE = 128;
   char buff[BUFFER_SIZE];
+  int pipe_id;
 
   int ret = mkfifo("named_pipe", 0664);
   if(-1 == ret)
   {
+    /* ignore this */
     printf("mkfifo() failed: %s!\r\n", strerror(errno));
-    return -1;
-  }
-
-  int pipe_id = open("named_pipe", O_CREAT | O_RDWR);
-  if(-1 == pipe_id)
-  {
-    printf("open named pipe failed: %s", strerror(errno));
-    return -2;
   }
 
   pid_t pid = fork();
-  if(0 == pid)
+  if(0 == pid) /* child process */
   {
-    int iReadNum = read(pipe_id, buff, BUFFER_SIZE);
-    printf("child process read %d B from pipe: %s", iReadNum, buff);
-
-    /* at this point, next read would block on my machine. */
-    // memset(buff, 0, BUFFER_SIZE);
-    // iReadNum = read(pipe_id, buff, BUFFER_SIZE);
-    // printf("child process second read %d B from pipe: %s", iReadNum, buff);
-    // fflush(stdout);
-
-    /* both ends open required, re-open wil be blocked */
-    close(pipe_id);
     pipe_id = open("named_pipe", O_RDONLY);
     if(-1 == pipe_id)
     {
       printf("open named pipe failed: %s", strerror(errno));
-      return -2;
+      return -1;
     }
 
-    printf("pipe_id: %d\r\n", pipe_id);
+    int iReadNum = read(pipe_id, buff, BUFFER_SIZE);
+    printf("child process read %d B from pipe: %s", iReadNum, buff);
+
+    /* at this point, next read would see EOF. */
+    memset(buff, 0, BUFFER_SIZE);
+    iReadNum = read(pipe_id, buff, BUFFER_SIZE);
+    printf("child process second read %d B from pipe: %s", iReadNum, buff);
+    fflush(stdout);
+
     close(pipe_id);
+
+    /* both ends open required, re-open wil be blocked */
+//    pipe_id = open("named_pipe", O_RDONLY);
+//    if(-1 == pipe_id)
+//    {
+//      printf("open named pipe failed: %s", strerror(errno));
+//      return -1;
+//    }
+//
+//    printf("pipe_id: %d\r\n", pipe_id);
+//    close(pipe_id);
 
     exit(EXIT_SUCCESS);
   }
-  else
+  else /* parent process */
   {
+    pipe_id = open("named_pipe", O_WRONLY);
+    if(-1 == pipe_id)
+    {
+      printf("open named pipe failed: %s", strerror(errno));
+      return -1;
+    }
+
     printf("input the text to be writen in pipe: ");
     (void)fgets(buff, BUFFER_SIZE, stdin);
 
